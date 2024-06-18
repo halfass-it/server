182936830a87bcee5c97b948533b84f03e8c6ae6
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-18 10:38:18 +0100

    sync

--- utils/setup.py
+++ utils/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pylogger',
+    name='utils',
     version='1.0.2',
     python_requires='>=3.6, <4.0',
     packages=find_packages(),
