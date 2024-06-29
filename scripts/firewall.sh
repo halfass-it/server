2e7ff15de045f035aa5a3e255c2d9c4c5bbdfa73
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-29 21:25:30 +0100

    fixed gateway and parser better type checking or die

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -1,23 +1,24 @@
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
+sudo ufw allow from 127.0.0.1 to any port 5000
 sudo ufw allow from 127.0.0.1 to any port 6000
 sudo ufw allow from 127.0.0.1 to any port 7000
 sudo ufw allow from 127.0.0.1 to any port 6001
 sudo ufw allow from 127.0.0.1 to any port 6002
 sudo ufw allow from 127.0.0.1 to any port 7001
 sudo ufw allow from 127.0.0.1 to any port 7002
-sudo ufw allow from 127.0.0.1 to any port 1337
-sudo ufw allow from 127.0.0.1 to any port 1338
-sudo ufw allow from 127.0.0.1 to any port 1339
-sudo ufw allow from 127.0.0.1 to any port 1340
+sudo ufw allow from 127.0.0.1 to any port 5001
+sudo ufw allow from 127.0.0.1 to any port 5002
+sudo ufw allow from 127.0.0.1 to any port 5003
+sudo ufw allow from 127.0.0.1 to any port 5004
+sudo ufw deny 5000/tcp
 sudo ufw deny 6000/tcp
 sudo ufw deny 7000/tcp
+sudo ufw deny 5001/tcp
+sudo ufw deny 5002/tcp
+sudo ufw deny 5003/tcp
+sudo ufw deny 5004/tcp
 sudo ufw deny 6001/tcp
 sudo ufw deny 6002/tcp
 sudo ufw deny 7001/tcp
 sudo ufw deny 7002/tcp
-sudo ufw deny 1337/tcp
-sudo ufw deny 1338/tcp
-sudo ufw deny 1339/tcp
-sudo ufw deny 1340/tcp
-
