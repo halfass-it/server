9d36e26236e877538b89540dcc9a9341f954e29f
rodfer0x80 <rodrigolf080@gmail.com>
2024-08-18 17:43:09 +0100

    fixed auth packet naming

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo systemctl stop nginx
-sudo kill -9 $(sudo fuser 5001/tcp 5002/tcp 5003/tcp 5004/tcp) >/dev/null 2>&1
-sudo kill -9 $(sudo fuser 6001/tcp 6002/tcp) >/dev/null 2>&1
-sudo kill -9 $(sudo fuser 7001/tcp 7002/tcp) >/dev/null 2>&1
+sudo kill -9 $(sudo fuser 5000/tcp 5001/tcp 5002/tcp 5003/tcp 5004/tcp) >/dev/null 2>&1
+sudo kill -9 $(sudo fuser 6000/tcp 6001/tcp 6002/tcp) >/dev/null 2>&1
+sudo kill -9 $(sudo fuser 7000/tcp 7001/tcp 7002/tcp) >/dev/null 2>&1
